1afe90c6ac174f7475825d9344a03aff2d4d0f23
LD\jkrause <jkrause@loandepot.com>
2017-12-26 14:44:28 -0800

    Added postMessage observable wrapper

--- src/services/utilities.service.ts
+++ src/services/utilities.service.ts
@@ -1,10 +1,46 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 
 @Injectable()
 export class UtilitiesService {
 
+	/** Holds postmessage response */
+	private postMessage$ = new BehaviorSubject(null);
+	/** Holds event listener for postMessage */
+	private postMessageListener;
+	/** The origin domain to whitelist incoming post messages */
+	private postMessageOrigin: string;
+
 	constructor(
-	) {}
+	) { }
+
+	/**
+	 * Listen to postMessage events
+	 * @param origin The origin domain that the post message is expected from for whitelisting. IE 'http://localhost:4200'
+	 */
+	public postMessageListen(origin: string) {
+		this.postMessageListener = window.addEventListener('message', this.messageReceived.bind(this), false); // Add an event listener
+		this.postMessageOrigin = origin; // Set origin
+		return this.postMessage$; // Return observable
+	}
+
+	/**
+	 * When a message is received from postMessage
+	 * @param event
+	 */
+	private messageReceived(event: MessageEvent) {
+		// Make sure this isn't a null message event
+		if (event.data && event.data.type != 'webpackOk') {
+			// If this is from the correct original url and has a token
+			if (event.origin == this.postMessageOrigin) { // && event.data.token
+				this.postMessage$.next(event.data.payload);
+			}
+			// Add some error messaging to check for origin
+			else if (event.origin != this.postMessageOrigin) {
+				console.error('This workstation was loaded from an unapproved client');
+			}
+		}
+	}
     
 	/**
 	* Copy the selected string to the clipboard
